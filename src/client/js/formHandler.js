88b556c262ab20e23b980d0bf33fd531be2c2ad3
Evgeny <evgeny90@gmail.com>
2021-11-15 23:33:48 -0600

    Changed formHandler validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,23 +2,16 @@ async function handleSubmit(event) {
     event.preventDefault();
 
     let url = document.getElementById("url").value;
-    let encodeUrl = encodeURIComponent(url);
+    let encodeUrl = encodeURIComponent(url) || undefined;
 
     console.log("::: Form Submitted :::");
 
     document.getElementById("error").innerHTML = "";
 
-    if (encodeUrl) {
-        const response = await fetch(`/getData/${encodeUrl}`);
-        const json = await response.json();
-        let msg = json.status.msg;
-        Client.formSubmitValidator(msg, json);
-    } else {
-        document.getElementById("error").innerHTML = "Please enter the valid URL";
-        document.getElementById("score_tag").innerHTML = "";
-        document.getElementById("confidence").innerHTML = "";
-        document.getElementById("irony").innerHTML = "";
-    }
+    const response = await fetch(`/getData/${encodeUrl}`);
+    const json = await response.json();
+    let msg = json.status.msg;
+    Client.formSubmitValidator(msg, json);
 }
 
 export { handleSubmit };
